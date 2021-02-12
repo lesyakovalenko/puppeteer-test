0193e9df353b5e179e92d51c6db903c6c8c8b757
lesyakovalenko <lesya.kovalenko.dev@gmail.com>
2021-02-12 18:08:36 +0200

    fix selectors

--- test.js
+++ test.js
@@ -1,67 +1,99 @@
 const puppeteer = require('puppeteer');
 require('dotenv').config();
-let url = 'https://www.facebook.com';
-
-let email = process.env.EMAIL;
-let pass = process.env.PASSWORD;
-
+const url = 'https://www.facebook.com';
+let urlProfile = '';//TODO add url profile example 'https://www.facebook.com/your.link/'
+const email = process.env.EMAIL;
+const pass = process.env.PASSWORD;
 (async () => {
     const browser = await puppeteer.launch({headless: false});
     let page = await browser.newPage();
-    await page.goto(url);
-    await page.focus('input[id=email]');
-    await page.keyboard.type(email)
-    await page.focus('input[id=pass]');
-    await page.keyboard.type(pass);
 
-    let res = await Promise.all([
-        await page.keyboard.press("Enter"),
-        await page.waitForNavigation({waitUntil: 'networkidle2'}),
-    ])
-    let partUrl = await page.evaluate(() => {
-        let link = document.querySelector('div[aria-label="Facebook"]>ul>li>span>div>a[href="/bookmarks/"]')
-        return link.getAttribute('href');
-    });
-    let newUrl = url.concat(partUrl);
-    await page.goto(newUrl, {waitUntil: 'networkidle2'});
-    let urlProfile = await page.evaluate(async () => {
-        let link = document.querySelector('div[data-pagelet="page"]>div>div>div[class="buofh1pr"]>ul>li>div>a');
-        return link.getAttribute('href');
-    });
-    await page.goto(urlProfile, {waitUntil: 'networkidle2'});
+    async function loginUser() {
+        await page.goto(url);
+        await page.focus('input[id=email]');
+        await page.keyboard.type(email)
+        await page.focus('input[id=pass]');
+        await page.keyboard.type(pass);
 
-    let user = await page.evaluate(() => {
-        let title = document.querySelector('div[class="bi6gxh9e aov4n071"]>span[dir="auto"]>h1[class="gmql0nx0 l94mrbxd p1ri9a11 lzcic4wl bp9cbjyn j83agx80"][dir="auto"]');
-        let name = title.textContent;
-        let img = document.querySelector('svg[aria-label]>g[mask="url(#jsc_c_i)"]>image');
-        const studySelector = 'div[data-pagelet="ProfileTilesFeed_0"] ul span'
-        let placeOfStudy = document.querySelector(studySelector).textContent
+        let res = await Promise.all([
+            await page.keyboard.press("Enter"),
+            await page.waitForNavigation({waitUntil: 'networkidle2'}),
+        ])
+    }
 
-        return {
-            name: name,
-            imgUrl: img?.hasAttribute('xlink:href') ? img.getAttribute('xlink:href') : 'img',
-            placeOfStudy: placeOfStudy
-        };
-    })
+    async function getInfoForLogoutUser() {
+        await page.goto(urlProfile, {waitUntil: 'networkidle2'});
+        let partUrl = await page.evaluate(() => {
+            let name = document.querySelector('div[id="fbProfileCover"] span[id="fb-timeline-cover-name"]>a');
+            let image = document.querySelector('div[id="fbTimelineHeadline"] img').getAttribute('src');
+            let isCheckEducation = document.querySelector('div[id="pagelet_eduwork"]');
+            let educations = []
+            if (isCheckEducation) {
+                let educationNodes = document.querySelectorAll('div[id="pagelet_eduwork"] ul li a:not([tabindex][aria-hidden])')
+                educationNodes.forEach(value => {
+                    educations.push(value.innerText)
+                })
+            }
 
-    console.log(`full Name: ${user.name}`);
-    console.log(`place of study: ${user.placeOfStudy}`);
-    console.log(`image: ${user.imgUrl}`);
+            console.log(name, image, educations);
+            return {
+                name: name.textContent,
+                imageUrl: image,
+                educations: educations
+            };
+        });
+        console.log('part url', partUrl);
+    }
 
-    let urlPosts = String(urlProfile).concat('posts/');
-    let selector = 'div[data-pagelet="ProfileTimeline"] a[href^="' + urlPosts + '"]';
+    async function getInfoForLoginUser() {
+        console.log(urlProfile)
+        if (!urlProfile || !urlProfile.length) {
+            let partUrl = await page.evaluate(() => {
+                let link = document.querySelector('div[aria-label="Facebook"]>ul>li>span>div>a[href="/bookmarks/"]')
+                return link.getAttribute('href');
+            });
+            let newUrl = url.concat(partUrl);
+            await page.goto(newUrl, {waitUntil: 'networkidle2'});
+            let profile = await page.evaluate(async () => {
+                let link = document.querySelector('div[data-pagelet="page"] ul>li>div>a');
+                return link.getAttribute('href');
+            });
+            urlProfile = String(profile)
+        }
+        await page.goto(urlProfile, {waitUntil: 'networkidle2'});
+        await page.focus('div[data-pagelet="root"] div[data-pagelet="page"] h1');
+        await page.keyboard.press("Enter");
+        let user = await page.evaluate(() => {
+            let title = document.querySelector('div[data-pagelet="root"] div[data-pagelet="page"] h1');
+            let name = title.textContent;
+            let img = document.querySelector('div[data-pagelet="root"] div[data-pagelet="page"] svg[aria-label]>g>image');
+            let isEducation = document.querySelector('div[data-pagelet="ProfileTilesFeed_0"] ul');
+            let education;
+            if (isEducation) {
+                education = document.querySelector('div[data-pagelet="ProfileTilesFeed_0"] ul span').textContent
+            }
 
-    await page.keyboard.press("Enter");
-    let length = 0
-    let timePosts;
-    while (length < 10) {
+            return {
+                name: name,
+                imgUrl: img?.hasAttribute('xlink:href') ? img.getAttribute('xlink:href') : 'img',
+                education: education
+
+            };
+        })
+
+        console.log(user)
+
+        let selector = 'div[data-pagelet="ProfileTimeline"] a span[aria-labelledby]>span[aria-labelledby]';
+
+        await page.keyboard.press("Enter");
+        let timePosts;
+
         await page.evaluate(async () => {
             const delay = 3000;
             const wait = (ms) => new Promise(res => setTimeout(res, ms));
-            const count = async () => document.querySelectorAll('div[data-pagelet="ProfileTimeline"]').length;
-            console.log(count)
+            const count = async () => document.querySelectorAll('div[data-pagelet="ProfileTimeline"]>div').length;
             const scrollDown = async () => {
-                document.querySelector('div[data-pagelet="ProfileTimeline"]:last-child')
+                document.querySelector('div[data-pagelet="ProfileTimeline"]>div:last-child')
                     .scrollIntoView({behavior: 'smooth', block: 'end', inline: 'center'});
             }
             let preCount = 0;
@@ -79,16 +111,27 @@ let pass = process.env.PASSWORD;
             let timeList = [];
             let listPosts = document.querySelectorAll(selector)
             listPosts.forEach(value => {
-                timeList.push(value.innerText);
+                let time = value.parentElement.innerText;
+                timeList.push(time);
             })
             return timeList;
 
         }, selector)
-        length = timePosts.length;
+        console.log(timePosts);
     }
-    console.log(timePosts);
+
+    if (pass && email) {
+        //TODO with credentials password and email
+        await loginUser();
+        await getInfoForLoginUser()
+    } else {
+        //TODO  if your logout but ...
+        await getInfoForLogoutUser();
+    }
     await browser.close()
 })()
+
+
 
 
 
